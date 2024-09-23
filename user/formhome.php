b03665dc8ca8e8c0af4efe9a16f8140ad7dccb00
fadillahiqbal123 <fadillahharianto163@gmail.com>
2024-09-23 16:32:12 +0700

    delete
    jck

--- user/formhome.php
+++ user/formhome.php
@@ -224,9 +224,7 @@
         </div>
     </div>
 
-    <footer>
-        alam jongkok
-    </footer>
+    
 
     <!-- Bootstrap JS Bundle -->
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js"></script>
