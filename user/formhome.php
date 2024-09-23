abec9b0ecc168148437a838764f518e3abc9f247
Lasticoo <lastico.alparesz@gmail.com>
2024-09-23 16:28:48 +0700

    alam jongkok
    footer

--- user/formhome.php
+++ user/formhome.php
@@ -224,6 +224,10 @@
         </div>
     </div>
 
+    <footer>
+        alam jongkok
+    </footer>
+
     <!-- Bootstrap JS Bundle -->
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js"></script>
     <!-- Custom JS -->
