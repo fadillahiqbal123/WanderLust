3304b0ca164c055fdfb5e27981e5953d04fc65ed
Lasticoo <lastico.alparesz@gmail.com>
2024-10-12 03:29:21 +0700

    pembaruan
    koneksi

--- admin/koneksi.php
+++ admin/koneksi.php
@@ -7,7 +7,6 @@ $nama_database = "db_wisata";
 $db = mysqli_connect($server, $user, $password, $nama_database);
 
 if ($db) {
-    echo "koneksi berhasil";
 }
 if ($db->connect_error) {
     die("Koneksi gagal " . $db->connect_error);
