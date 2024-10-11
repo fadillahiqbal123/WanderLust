86fd9157cfcad350f0d06b1671ad1bc6b5a57387
Lasticoo <lastico.alparesz@gmail.com>
2024-10-12 05:50:06 +0700

    perubahan dikit

--- admin/modul/mod_pengunjung/pengunjung.php
+++ admin/modul/mod_pengunjung/pengunjung.php
@@ -1,43 +1,43 @@
 <div class="container-fluid">
     <div class="card mt-4">
-    <div class="card-header">
-        <a href="#" class="btn btn-outline-primary"><i class="bi bi-plus-lg"></i>Data Wisata</a>
-    </div>
-    <div class="card-body">
-        <table id="example" class="display" style="width: 100%;">
-            <thead>
-                <tr>
-                    <th>No</th>
-                    <th>Nama Admin</th>
-                    <th>Email</th>
-                    <th>Username</th>
-                    <th>Password</th>
-                    <th>Aksi</th>
-                </tr>
-            </thead>
-            <tbody>
-                <?php  
+        <div class="card-header">
+            <a href="#" class="btn btn-outline-primary"><i class="bi bi-plus-lg"></i>Data Pengguna</a>
+        </div>
+        <div class="card-body">
+            <table id="example" class="display" style="width: 100%;">
+                <thead>
+                    <tr>
+                        <th>No</th>
+                        <th>Nama Pengguna</th>
+                        <th>Email</th>
+                        <th>Username</th>
+                        <th>Password</th>
+                        <th>Aksi</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <?php
                     $sql = mysqli_query($db, "SELECT * FROM user");
-                    while($r= mysqli_fetch_array($sql)){
-                ?>
-                <tr>
-                    <td></td>
-                    <td><?php echo $r['nama_user'] ?></td>
-                    <td><?php echo $r['email']?></td>
-                    <td><?php echo $r['username'] ?></td>
-                    <td><?php echo $r['password']?></td>
-                <td>
-                <a href="dashboard.php?hal=edit_wisata&id=<?php echo $r['id_user'] ?>" class="btn btn-outline-warning"><i class="bi bi-pencil-square"></i></a>
+                    while ($r = mysqli_fetch_array($sql)) {
+                    ?>
+                        <tr>
+                            <td></td>
+                            <td><?php echo $r['nama_user'] ?></td>
+                            <td><?php echo $r['email'] ?></td>
+                            <td><?php echo $r['username'] ?></td>
+                            <td><?php echo $r['password'] ?></td>
+                            <td>
+                                <a href="dashboard.php?hal=edit_wisata&id=<?php echo $r['id_user'] ?>" class="btn btn-outline-warning"><i class="bi bi-pencil-square"></i></a>
 
-                <a href="dashboard.php?hal=hapus_wisata&id=<?php echo $r['id_user'] ?>" class="btn btn-outline-danger"><i class="bi bi-trash3-fill"></i></a>
-                </td>
-                </tr>
-                <?php  
+                                <a href="dashboard.php?hal=hapus_wisata&id=<?php echo $r['id_user'] ?>" class="btn btn-outline-danger"><i class="bi bi-trash3-fill"></i></a>
+                            </td>
+                        </tr>
+                    <?php
                     }
-                ?>
+                    ?>
                 </tbody>
-            </tbody>
-        </table>
-    </div>
+                </tbody>
+            </table>
+        </div>
     </div>
 </div>
--- admin/modul/mod_user/user.php
+++ admin/modul/mod_user/user.php
@@ -3,53 +3,52 @@
         <div class="card-header"><strong>Pengaturan Akun</strong></div>
         <div class="card-body">
             <form class="action" method="POST">
-                
-            <div class="mb-3 w-25">
+
+                <div class="mb-3 w-25">
                     <label class="form-label">Nama Admin</label>
-                    <input type="text" name="nama" class="form-control" placeholder="Masukan Nama Admin" autocomplete="off" required/> 
+                    <input type="text" name="nama" class="form-control" placeholder="Masukan Nama Admin" autocomplete="off" required />
                 </div>
                 <div class="mb-3 w-25">
                     <label class="form-label">Email</label>
-                    <input type="text" name="username" class="form-control" placeholder="Masukan Username Admin" autocomplete="off" required/>
-                   
+                    <input type="text" name="username" class="form-control" placeholder="Masukan Username Admin" autocomplete="off" required />
+
                 </div>
                 <div class="mb-3 w-25">
                     <label class="form-label">Username</label>
-                    <input type="text" class="form-control" name="telfon"  placeholder="Masukan No Telefon Admin" required/>
+                    <input type="text" class="form-control" name="telfon" placeholder="Masukan No Telefon Admin" required />
                 </div>
                 <div class="mb-3 w-25">
                     <label class="form-label">Password</label>
-                    <input type="text"  class="form-control" name="alamat" placeholder="Masukan Alamat Admin" required/>
+                    <input type="text" class="form-control" name="alamat" placeholder="Masukan Alamat Admin" required />
                 </div>
                 <div class="mb-3 w-25">
                     <label for="" class="form-label">Konfirmasi Password</label>
-                    <input type="password"  class="form-control" name="konfirmasi_password" placeholder="Konfirmasi Password Admin" required/>
+                    <input type="password" class="form-control" name="konfirmasi_password" placeholder="Konfirmasi Password Admin" required />
                 </div>
                 <button type="submit" name="submit" class="btn btn-danger">Submit</button>
-                
+
             </form>
-            <?php  
-            
-            if(isset($_POST['submit'])){
+            <?php
+
+            if (isset($_POST['submit'])) {
                 $namaadmin            = $_POST['nama_admin'];
                 $email                = $_POST['email'];
                 $username             = $_POST['username'];
                 $password             = $_POST['password'];
                 $konfirmasi_password  = $_POST['konfirmasi_password'];
                 $id_user_login            = $_SESSION['idadmin'];
-   
-                if($password == $konfirmasi_password){
-                    $password_md5= md5($password);
 
-                   mysqli_query($db, "UPDATE admin SET username='$username', password='$password_md5', email='$email' WHERE id_admin= $id_user_login");
-                   
-                   echo "<script>alert('Data Berhasil Diubah'); window.location = 'dashboard.php?hal=user' </script";
+                if ($password == $konfirmasi_password) {
+                    $password_md5 = md5($password);
 
-                }else{
+                    mysqli_query($db, "UPDATE admin SET username='$username', password='$password_md5', email='$email' WHERE id_admin= $id_user_login");
+
+                    echo "<script>alert('Data Berhasil Diubah'); window.location = 'dashboard.php?hal=user' </script";
+                } else {
                     echo "<script>alert('Password Tidak Sama') window.location = 'dashboard.php?hal=user' </script>";
                 }
             }
-            
+
             ?>
         </div>
     </div>
